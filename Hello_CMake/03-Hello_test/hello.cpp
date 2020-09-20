41968e6222d9df4c2947e2f56581d99a340d1202
iotboy <iotboy@163.com>
2020-09-20 08:27:47 +0800

    Initial commit

---
+++ Hello_CMake/03-Hello_test/hello.cpp
@@ -0,0 +1,5 @@
+#include"hello.h"
+#include<iostream>
+void hello(std::string const& name){
+   std::cout << "Hello, " << name << "!\n";
+}
---
+++ Hello_CMake/08-properties_options/src/hello.h
@@ -0,0 +1,7 @@
+#ifndef HELLO_H
+#define HELLO_h
+#include <string>
+
+void hello(std::string const& name);
+
+#endif
